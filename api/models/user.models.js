ea2fff1587abd9462e9e5262fea39fc6093b3c8e
Sacarima <caroline12alex@yahoo.com>
2023-11-30 12:17:09 +0100

    create user model

---
+++ api/models/user.models.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+
+    password: {
+        type: String,
+        require: true,
+    }
+}, {timestamps: true}) // it let mongoDB time of user creation and update
+
+// MODEL
+
+const User = mongoose.model('User', userSchema)
+
+export default User
