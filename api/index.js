faabd9f382567a28b221053cacc34a7528ed6895
Sacarima <caroline12alex@yahoo.com>
2023-11-30 11:11:49 +0100

    database connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose.connect(process.env.DB_STRING)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+  });
 
 const app = express()
 
-app.listen(8080, () => {
-    console.log('Server is running on port 8080')
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
 })
