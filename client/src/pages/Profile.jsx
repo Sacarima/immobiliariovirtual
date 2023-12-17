1b2d2065616c36a2ce8b56c49128bd5b03b908f0
Sacarima <caroline12alex@yahoo.com>
2023-12-17 03:25:24 +0100

    complete update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,35 @@
 import { useSelector } from 'react-redux'
-import { useRef, useState, useEffect } from 'react'
-import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
+import { 
+  useRef, 
+  useState, 
+  useEffect 
+} from 'react'
+import { 
+  getStorage, 
+  ref, 
+  uploadBytesResumable, 
+  getDownloadURL 
+  } from 'firebase/storage'
+import { 
+  updateUserStart, 
+  updateUserSuccess,
+  updateUserFailure
+  
+} from '../redux/user/userSlice'
+import { useDispatch } from 'react-redux'
 import { app } from '../firebase'
 import { Link } from 'react-router-dom'
 
 
 const  Profile = () => {
   const fileRef = useRef(null)
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined)
   const [filePerc, setFilePerc] = useState(0)
   const [fileUploadError, setFileUploadError] = useState(false)
   const [formData, setFormData] = useState({})
-
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch()
 
   useEffect(() => {
     if (file) {
@@ -43,27 +60,39 @@ const  Profile = () => {
       }
     );
   };
-  // const handleFileUpload = (file) => {
-  //   const storage = getStorage(app)
-  //   const fileName = new Date().getTime() + file.name
-  //   const storageRef = ref(storage, fileName)
-  //   const uploadTask = uploadBytesResumable(storageRef, file)
 
-  //   uploadTask.on(
-  //     'state_changed', 
-  //     (snapshot) => {
-  //       const progress = 
-  //       (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-  //       setFilePerc(Math.round(progress))
-  //     }),
-  //     (error) =>{
-  //       setFileUploadError(true)
-  //     }
-  // }
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      })
+      const data = await res.json()
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message))
+        return
+      }
+
+      dispatch(updateUserSuccess(data))
+      setUpdateSuccess(true)
+    } catch (error) {
+      dispatch(updateUserFailure(error.message))
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form  className='flex flex-col gap-4'>
+      <form  onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input 
           type="file" 
           ref={fileRef} 
@@ -95,25 +124,30 @@ const  Profile = () => {
           type='text'
           placeholder='username'
           id='username'
+          defaultValue={currentUser.username}
           className='border p-3 rounded-lg'
+          onChange={handleChange}
         />
         <input
           type='email'
           placeholder='email'
           id='email'
+          defaultValue={currentUser.email}
           className='border p-3 rounded-lg'
+          onChange={handleChange}
         />
         <input
           type='password'
           placeholder='password'
           id='password'
           className='border p-3 rounded-lg'
+          onChange={handleChange}
         />
         <button
-        
+          disabled={loading}
           className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
         >
-          Update
+          {loading ? 'Loading...' : 'Update'}
         </button>
       </form>
       <div className='flex justify-between mt-5'>
@@ -126,6 +160,9 @@ const  Profile = () => {
           Sign out
         </span>
       </div>
+
+      <p className='text-red-700 mt-5'>{error? error : ''}</p>
+      <p className='text-green-700 mt-5'>{updateSuccess? 'Profile updated successfully' : ''}</p>
     </div>
   );
 }
