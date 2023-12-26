a1250dfe59dff4b9cdd0476cb68e2f35d7d7e091
Sacarima <caroline12alex@yahoo.com>
2023-12-26 12:03:51 +0100

    complete upload listing images functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -4,6 +4,7 @@ import {FaEnvelope, FaLock, FaSearch, FaUser} from 'react-icons/fa'
 import OAuth from '../components/OAuth'
 
 
+
 const SignUp = () => {
 
   const [formData, setFormData] = useState({})
@@ -48,59 +49,65 @@ const SignUp = () => {
   
   return (
     <div className='p-3 max-w-lg mx-auto'>
-        <div className='border rounded-lg py-6 px-8 pb-20 mt-20 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]'>
-          <h1 className='text-3xl text-center font-semibold  mt-7 mb-7'>Sign Up</h1>
+        <div>
+           
+        </div>
+
+        <div>
+          <div className='border rounded-lg py-6 px-8 pb-20 mt-20 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]'>
+            <h1 className='text-3xl text-center font-semibold  mt-7 mb-7'>Sign Up</h1>
           
-          <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
-              <label className="flex items-center gap-2 border-b focus:border-b-slate-600 mt-3">
-                    
-                    <FaUser className=" text-slate-600" />
-                    <input
-                        type="text"
-                        placeholder='Username'
-                        className='border p-3 rounded-lg outline-none bg-transparent border-none flex-auto'
-                        onChange={handleChange}
-                        id='username'
-                    />
-              </label>
-              <label className="flex items-center gap-2 border-b focus:border-b-slate-600">
-                    <FaEnvelope className=" text-slate-600" />
-                    <input
-                        type="email"
-                        placeholder='Email'
-                        className='border p-3 rounded-lg outline-none bg-transparent border-none flex-auto'
-                        onChange={handleChange}
-                        id='email'
-                    />
-              </label>
-                <label className="flex items-center gap-2 border-b">
-                    <FaLock className=" text-slate-600" />
-                    <input
-                        type="password"
-                        placeholder='Password'
-                        className=' p-3 rounded-lg outline-none  border-none focus:bg-none flex-auto'
-                        onChange={handleChange}
-                        id='password'
-                    />
+            <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
+                <label className="flex items-center gap-2 border-b focus:border-b-slate-600 mt-3">
           
+                      <FaUser className=" text-slate-600" />
+                      <input
+                          type="text"
+                          placeholder='Username'
+                          className='border p-3 rounded-lg outline-none bg-transparent border-none flex-auto'
+                          onChange={handleChange}
+                          id='username'
+                      />
                 </label>
+                <label className="flex items-center gap-2 border-b focus:border-b-slate-600">
+                      <FaEnvelope className=" text-slate-600" />
+                      <input
+                          type="email"
+                          placeholder='Email'
+                          className='border p-3 rounded-lg outline-none bg-transparent border-none flex-auto'
+                          onChange={handleChange}
+                          id='email'
+                      />
+                </label>
+                  <label className="flex items-center gap-2 border-b">
+                      <FaLock className=" text-slate-600" />
+                      <input
+                          type="password"
+                          placeholder='Password'
+                          className=' p-3 rounded-lg outline-none  border-none focus:bg-none flex-auto'
+                          onChange={handleChange}
+                          id='password'
+                      />
           
-              <button
-                disabled={loading}
-                className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 outline-none mt-6'
-              >
-                  {loading ? 'Loading...' : 'Sign up'}
-              </button>
-              <OAuth />
-                </form>
-                
-                <div className='flex gap-2 mt-5'>
-          <p>Have an account?</p>
-          <Link to={"/sign-in"}>
-            <span className='text-blue-700'>Sign in</span>
-          </Link>
-                </div>
-                {error && <p className='text-red-500 mt-5'>{error}</p>}
+                  </label>
+          
+                <button
+                  disabled={loading}
+                  className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 outline-none mt-6'
+                >
+                    {loading ? 'Loading...' : 'Sign up'}
+                </button>
+                <OAuth />
+                  </form>
+          
+                  <div className='flex gap-2 mt-5'>
+            <p>Have an account?</p>
+            <Link to={"/sign-in"}>
+              <span className='text-blue-700'>Sign in</span>
+            </Link>
+                  </div>
+                  {error && <p className='text-red-500 mt-5'>{error}</p>}
+          </div>
         </div>
     </div>
   )
