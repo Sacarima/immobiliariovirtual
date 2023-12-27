c4bfaeffb5f415a2a8a28e1ff691a64d48173722
Sacarima <caroline12alex@yahoo.com>
2023-12-27 13:56:01 +0100

    complete create listing functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,14 +1,31 @@
 import { useState } from "react"
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage"
 import { app } from "../firebase"
+import { useSelector } from "react-redux"
+import { useNavigate } from 'react-router-dom';
 
 
  const CreateListing = () => {
-  const [files, setFiles] = useState([])
+  const { currentUser } = useSelector((state) => state.user)
   const [imageUploadError, setImageUploadError] = useState(false)
-  const [uploading, setUploading] = useState(false);
+  const [uploading, setUploading] = useState(false)
+  const navigate = useNavigate()
+  const [files, setFiles] = useState([])
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
   const [formData, setFormData] = useState({
     imageUrls: [],
+    name: '',
+    description: '',
+    address: '',
+    type: 'rent',
+    bedrooms: 1,
+    bathrooms: 1,
+    regularPrice: 50,
+    discountPrice: 0,
+    offer: false,
+    parking: false,
+    furnished: false,
   })
 
   console.log(formData)
@@ -74,12 +91,79 @@ import { app } from "../firebase"
     });
   };
 
+
+  const handleChange = (e) => {
+    if (e.target.id === 'sale' || e.target.id === 'rent') {
+      setFormData({
+        ...formData,
+        type: e.target.id,
+      })
+    }
+
+    if (
+      e.target.id === 'parking' ||
+      e.target.id === 'furnished' ||
+      e.target.id === 'offer'
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.checked,
+      })
+    }
+
+    if (
+      e.target.type === 'number' ||
+      e.target.type === 'text' ||
+      e.target.type === 'textarea'
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      })
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (formData.imageUrls.length < 1)
+        return setError('You must upload at least one image');
+      if (+formData.regularPrice < +formData.discountPrice)
+        return setError('Discount price must be lower than regular price');
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/listing/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...formData,
+          userRef: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+      }
+      navigate(`/listing/${data._id}`); // redirect to listing page
+      
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <main className='p-3 max-w-4xl mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>
         Create a Listing
       </h1>
-      <form  className='flex flex-col sm:flex-row gap-4'>
+      <form 
+         className='flex flex-col sm:flex-row gap-4'
+         onSubmit={handleSubmit}
+      >
         <div className='flex flex-col gap-4 flex-1'>
           <input
             type='text'
@@ -89,6 +173,8 @@ import { app } from "../firebase"
             maxLength='62'
             minLength='10'
             required
+            onChange={handleChange}
+            value={formData.name}
             
           />
           <textarea
@@ -97,6 +183,8 @@ import { app } from "../firebase"
             className='border p-3 rounded-lg'
             id='description'
             required
+            onChange={handleChange}
+            value={formData.description}
             
           />
           <input
@@ -105,6 +193,8 @@ import { app } from "../firebase"
             className='border p-3 rounded-lg'
             id='address'
             required
+            onChange={handleChange}
+            value={formData.address}
             
           />
           <div className='flex gap-6 flex-wrap'>
@@ -113,6 +203,8 @@ import { app } from "../firebase"
                 type='checkbox'
                 id='sale'
                 className='w-5'
+                onChange={handleChange}
+                checked={formData.type === 'sale'}
                 
               />
               <span>Sell</span>
@@ -122,6 +214,8 @@ import { app } from "../firebase"
                 type='checkbox'
                 id='rent'
                 className='w-5'
+                onChange={handleChange}
+                checked={formData.type === 'rent'}
                 
               />
               <span>Rent</span>
@@ -131,6 +225,8 @@ import { app } from "../firebase"
                 type='checkbox'
                 id='parking'
                 className='w-5'
+                onChange={handleChange}
+                checked={formData.parking}
                 
               />
               <span>Parking spot</span>
@@ -140,6 +236,8 @@ import { app } from "../firebase"
                 type='checkbox'
                 id='furnished'
                 className='w-5'
+                onChange={handleChange}
+                checked={formData.furnished}
                 
               />
               <span>Furnished</span>
@@ -149,6 +247,8 @@ import { app } from "../firebase"
                 type='checkbox'
                 id='offer'
                 className='w-5'
+                onChange={handleChange}
+                checked={formData.offer}
                 
               />
               <span>Offer</span>
@@ -163,6 +263,8 @@ import { app } from "../firebase"
                 max='10'
                 required
                 className='p-3 border border-gray-300 rounded-lg'
+                onChange={handleChange}
+                value={formData.bedrooms}
                 
               />
               <p>Beds</p>
@@ -175,6 +277,8 @@ import { app } from "../firebase"
                 max='10'
                 required
                 className='p-3 border border-gray-300 rounded-lg'
+                onChange={handleChange}
+                value={formData.bathrooms}
                 
               />
               <p>Baths</p>
@@ -187,27 +291,36 @@ import { app } from "../firebase"
                 max='10000000'
                 required
                 className='p-3 border border-gray-300 rounded-lg'
+                onChange={handleChange}
+                value={formData.regularPrice}
                 
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
                  <span className='text-xs'>($ / month)</span>
               </div>
-            </div>
-              <div className='flex items-center gap-2'>
-                <input
-                  type='number'
-                  id='discountPrice'
-                  min='0'
-                  max='10000000'
-                  required
-                  className='p-3 border border-gray-300 rounded-lg'
-                />
-                <div className='flex flex-col items-center'>
-                  <p>Discounted price</p>
-                  <span className='text-xs'>($ / month)</span>
-                </div>
               </div>
+              {formData.offer && (
+                  <div className='flex items-center gap-2'>
+                    <input
+                      type='number'
+                      id='discountPrice'
+                      min='0'
+                      max='100000000'
+                      required
+                      className='p-3 border border-gray-300 rounded-lg'
+                      onChange={handleChange}
+                      value={formData.discountPrice}
+
+                    />
+                    <div className='flex flex-col items-center'>
+                      <p>Discounted price</p>
+                      <span className='text-xs'>($ / month)</span>
+                    </div>
+                  </div>
+              )}
+
+              
           </div>
         </div>
         <div className='flex flex-col flex-1 gap-4'>
@@ -259,10 +372,12 @@ import { app } from "../firebase"
             ))
           }
           <button
+            disabled={loading || uploading}
             className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
           >
-            Create listing
+            {loading ? 'Creating...' : 'Create listing'}
           </button>
+          {error && <p className='text-red-700 text-sm'>{error}</p>}
         </div>
         
       </form>
