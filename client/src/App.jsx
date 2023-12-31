900a97d5974538ca18e33a52271038ad1dc57b88
Sacarima <caroline12alex@yahoo.com>
2023-12-31 19:37:22 +0100

    update listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import About from './pages/About'
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
+import UpdateListing from './pages/UpdateListing'
 
 
  const App = () => {
@@ -21,6 +22,7 @@ import CreateListing from './pages/CreateListing'
           <Route element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
             <Route path='/create-listing' element={<CreateListing />} />
+            <Route path='/update-listing/:listingId' element={<UpdateListing />} />
           </Route>
       </Routes>
     </BrowserRouter>
--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -1,18 +1,19 @@
-import { useState } from "react"
-import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage"
-import { app } from "../firebase"
-import { useSelector } from "react-redux"
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
 
-
- const CreateListing = () => {
-  const { currentUser } = useSelector((state) => state.user)
-  const [imageUploadError, setImageUploadError] = useState(false)
-  const [uploading, setUploading] = useState(false)
-  const navigate = useNavigate()
-  const [files, setFiles] = useState([])
-  const [error, setError] = useState(false)
-  const [loading, setLoading] = useState(false)
+export default function CreateListing() {
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const params = useParams();
+  const [files, setFiles] = useState([]);
   const [formData, setFormData] = useState({
     imageUrls: [],
     name: '',
@@ -26,15 +27,32 @@ import { useNavigate } from 'react-router-dom';
     offer: false,
     parking: false,
     furnished: false,
-  })
+  });
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  console.log(formData)
-  
+  useEffect(() => {
+    const fetchListing = async () => {
+      const listingId = params.listingId;
+      const res = await fetch(`/api/listing/get/${listingId}`);
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setFormData(data);
+    };
+
+    fetchListing();
+  }, []);
+
   const handleImageSubmit = (e) => {
     if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
-      setUploading(true)
-      setImageUploadError(false)
-      const promises = []
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = [];
 
       for (let i = 0; i < files.length; i++) {
         promises.push(storeImage(files[i]));
@@ -45,44 +63,43 @@ import { useNavigate } from 'react-router-dom';
             ...formData,
             imageUrls: formData.imageUrls.concat(urls),
           });
-          setImageUploadError(false)
-          setUploading(false)
+          setImageUploadError(false);
+          setUploading(false);
         })
         .catch((err) => {
           setImageUploadError('Image upload failed (2 mb max per image)');
-          setUploading(false)
+          setUploading(false);
         });
     } else {
       setImageUploadError('You can only upload 6 images per listing');
-      setUploading(false)
+      setUploading(false);
     }
-  }
+  };
 
   const storeImage = async (file) => {
     return new Promise((resolve, reject) => {
-      const storage = getStorage(app)
-      const fileName = new Date().getTime() + file.name
-      const storageRef = ref(storage, fileName)
-      const uploadTask = uploadBytesResumable(storageRef, file)
-
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
       uploadTask.on(
         'state_changed',
         (snapshot) => {
           const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100 
-          console.log(`Upload is ${progress}% done`)
-         },
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+        },
         (error) => {
-          reject(error)
+          reject(error);
         },
         () => {
           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            resolve(downloadURL)
-          })
+            resolve(downloadURL);
+          });
         }
-      )
-    })
-  }
+      );
+    });
+  };
 
   const handleRemoveImage = (index) => {
     setFormData({
@@ -91,13 +108,12 @@ import { useNavigate } from 'react-router-dom';
     });
   };
 
-
   const handleChange = (e) => {
     if (e.target.id === 'sale' || e.target.id === 'rent') {
       setFormData({
         ...formData,
         type: e.target.id,
-      })
+      });
     }
 
     if (
@@ -108,7 +124,7 @@ import { useNavigate } from 'react-router-dom';
       setFormData({
         ...formData,
         [e.target.id]: e.target.checked,
-      })
+      });
     }
 
     if (
@@ -119,9 +135,9 @@ import { useNavigate } from 'react-router-dom';
       setFormData({
         ...formData,
         [e.target.id]: e.target.value,
-      })
+      });
     }
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -132,7 +148,7 @@ import { useNavigate } from 'react-router-dom';
         return setError('Discount price must be lower than regular price');
       setLoading(true);
       setError(false);
-      const res = await fetch('/api/listing/create', {
+      const res = await fetch(`/api/listing/update/${params.listingId}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -147,23 +163,18 @@ import { useNavigate } from 'react-router-dom';
       if (data.success === false) {
         setError(data.message);
       }
-      navigate(`/listing/${data._id}`); // redirect to listing page
-      
+      navigate(`/listing/${data._id}`);
     } catch (error) {
       setError(error.message);
       setLoading(false);
     }
   };
-
   return (
     <main className='p-3 max-w-4xl mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>
-        Create a Listing
+        Update a Listing
       </h1>
-      <form 
-         className='flex flex-col sm:flex-row gap-4'
-         onSubmit={handleSubmit}
-      >
+      <form onSubmit={handleSubmit} className='flex flex-col sm:flex-row gap-4'>
         <div className='flex flex-col gap-4 flex-1'>
           <input
             type='text'
@@ -175,7 +186,6 @@ import { useNavigate } from 'react-router-dom';
             required
             onChange={handleChange}
             value={formData.name}
-            
           />
           <textarea
             type='text'
@@ -185,7 +195,6 @@ import { useNavigate } from 'react-router-dom';
             required
             onChange={handleChange}
             value={formData.description}
-            
           />
           <input
             type='text'
@@ -195,7 +204,6 @@ import { useNavigate } from 'react-router-dom';
             required
             onChange={handleChange}
             value={formData.address}
-            
           />
           <div className='flex gap-6 flex-wrap'>
             <div className='flex gap-2'>
@@ -205,7 +213,6 @@ import { useNavigate } from 'react-router-dom';
                 className='w-5'
                 onChange={handleChange}
                 checked={formData.type === 'sale'}
-                
               />
               <span>Sell</span>
             </div>
@@ -216,7 +223,6 @@ import { useNavigate } from 'react-router-dom';
                 className='w-5'
                 onChange={handleChange}
                 checked={formData.type === 'rent'}
-                
               />
               <span>Rent</span>
             </div>
@@ -227,7 +233,6 @@ import { useNavigate } from 'react-router-dom';
                 className='w-5'
                 onChange={handleChange}
                 checked={formData.parking}
-                
               />
               <span>Parking spot</span>
             </div>
@@ -238,7 +243,6 @@ import { useNavigate } from 'react-router-dom';
                 className='w-5'
                 onChange={handleChange}
                 checked={formData.furnished}
-                
               />
               <span>Furnished</span>
             </div>
@@ -249,7 +253,6 @@ import { useNavigate } from 'react-router-dom';
                 className='w-5'
                 onChange={handleChange}
                 checked={formData.offer}
-                
               />
               <span>Offer</span>
             </div>
@@ -265,7 +268,6 @@ import { useNavigate } from 'react-router-dom';
                 className='p-3 border border-gray-300 rounded-lg'
                 onChange={handleChange}
                 value={formData.bedrooms}
-                
               />
               <p>Beds</p>
             </div>
@@ -279,7 +281,6 @@ import { useNavigate } from 'react-router-dom';
                 className='p-3 border border-gray-300 rounded-lg'
                 onChange={handleChange}
                 value={formData.bathrooms}
-                
               />
               <p>Baths</p>
             </div>
@@ -293,34 +294,34 @@ import { useNavigate } from 'react-router-dom';
                 className='p-3 border border-gray-300 rounded-lg'
                 onChange={handleChange}
                 value={formData.regularPrice}
-                
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                 <span className='text-xs'>($ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}
               </div>
+            </div>
+            {formData.offer && (
+              <div className='flex items-center gap-2'>
+                <input
+                  type='number'
+                  id='discountPrice'
+                  min='0'
+                  max='10000000'
+                  required
+                  className='p-3 border border-gray-300 rounded-lg'
+                  onChange={handleChange}
+                  value={formData.discountPrice}
+                />
+                <div className='flex flex-col items-center'>
+                  <p>Discounted price</p>
+                  {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                  )}
+                </div>
               </div>
-              {formData.offer && (
-                  <div className='flex items-center gap-2'>
-                    <input
-                      type='number'
-                      id='discountPrice'
-                      min='0'
-                      max='100000000'
-                      required
-                      className='p-3 border border-gray-300 rounded-lg'
-                      onChange={handleChange}
-                      value={formData.discountPrice}
-
-                    />
-                    <div className='flex flex-col items-center'>
-                      <p>Discounted price</p>
-                      <span className='text-xs'>($ / month)</span>
-                    </div>
-                  </div>
-              )}
-
-              
+            )}
           </div>
         </div>
         <div className='flex flex-col flex-1 gap-4'>
@@ -332,34 +333,35 @@ import { useNavigate } from 'react-router-dom';
           </p>
           <div className='flex gap-4'>
             <input
+              onChange={(e) => setFiles(e.target.files)}
               className='p-3 border border-gray-300 rounded w-full file:mr-3 file:py-2 file:px-4 hover:file:opacity-75 text-slate-700 file:rounded-md file:border-0 file:cursor-pointer'
               type='file'
               id='images'
               accept='image/*'
               multiple
-              onChange={(e) => setFiles(e.target.files)}
             />
             <button
-              onClick={handleImageSubmit}
               type='button'
               disabled={uploading}
-
+              onClick={handleImageSubmit}
               className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'
             >
               {uploading ? 'Uploading...' : 'Upload'}
             </button>
           </div>
-          <p className="text-red-700">{imageUploadError && imageUploadError}</p>
-          {
-            formData.imageUrls.length > 0 && formData.imageUrls.map((url, index) => (
-              <div 
-              key={url}
-              className='flex justify-between p-3 border items-center'
+          <p className='text-red-700 text-sm'>
+            {imageUploadError && imageUploadError}
+          </p>
+          {formData.imageUrls.length > 0 &&
+            formData.imageUrls.map((url, index) => (
+              <div
+                key={url}
+                className='flex justify-between p-3 border items-center'
               >
-                <img 
-                  src={url} 
-                  alt="listing image"
-                  className="w-20 h-20 object-contain rounded-lg"
+                <img
+                  src={url}
+                  alt='listing image'
+                  className='w-20 h-20 object-contain rounded-lg'
                 />
                 <button
                   type='button'
@@ -369,21 +371,16 @@ import { useNavigate } from 'react-router-dom';
                   Delete
                 </button>
               </div>
-            ))
-          }
+            ))}
           <button
             disabled={loading || uploading}
             className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
           >
-            {loading ? 'Creating...' : 'Create listing'}
+            {loading ? 'Updating...' : 'Update listing'}
           </button>
           {error && <p className='text-red-700 text-sm'>{error}</p>}
         </div>
-        
       </form>
     </main>
   );
 }
-
-
-export default CreateListing
